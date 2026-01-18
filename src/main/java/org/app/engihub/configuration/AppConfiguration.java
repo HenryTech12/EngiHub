637169b241514b187edd4af75e9c43eedbaae4dd
HenryTech12 <fakorodehenry@gmail.com>
2026-01-18 21:11:11 +0100

    created app configurations

--- src/main/java/org/app/engihub/configuration/AppConfiguration.java
+++ src/main/java/org/app/engihub/configuration/AppConfiguration.java
@@ -1,10 +1,20 @@
 package org.app.engihub.configuration;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class AppConfiguration {
 
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
